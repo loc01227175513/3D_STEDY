6442dbe71077d46c2d361c885557aff564a79d6b
ĐỖ Ngọc Lộc <51218059+loc01227175513@users.noreply.github.com>
2025-03-19 21:32:03 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Về chúng tôi</h1>
       <p className="text-gray-600">
-        Đây là trang giới thiệu về 3D STEDY
+        Đây là trang giới thiệu về 3D STEDY ANH Loc Dep Trai
       </p>
     </div>
   );
