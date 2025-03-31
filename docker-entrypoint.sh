c6192fd77970d6400d946875408b6fac82582bb1
ĐỖ Ngọc Lộc <51218059+loc01227175513@users.noreply.github.com>
2025-03-31 15:56:52 +0700

    up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 echo "Starting container setup..."
 
 # This is important: we do NOT want to replace the API domain in the NGINX configuration
-# The front-end is served on 64.23.206.54:3000 but should make API calls to 137.184.13.30:3000
+# The front-end is served on port 3000 but should make API calls to 137.184.13.30:3000
 echo "Using API domain: http://137.184.13.30:3000"
 
 # Ensure permissions are correct
